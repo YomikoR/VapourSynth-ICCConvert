e0e81d1915776efb453c58475c9bd45fac86fe79
YomikoR <monommino@gmail.com>
2021-09-24 11:42:30 +0800

    magick: iccc should never be built depending on im

--- src/magick/magick.hpp
+++ src/magick/magick.hpp
@@ -3,13 +3,6 @@
 
 #include "../common.hpp"
 
-#if defined(HAVE_MAGICK)
-#include <Magick++.h>
-#ifndef MAGICKCORE_LCMS_DELEGATE
-# error ImageMagick is not built with Little CMS support.
-#endif
-#endif // HAVE_MAGICK
-
 // for Windows we will build a standalone DLL linking ImageMagick
 #if defined(_WIN32)
 #define EXPORT_WHEN_W32(ret) VS_EXTERNAL_API(ret)
